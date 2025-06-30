ee4c53f8956a7bf855d06159e85a2cf1adda76f9
arRahmouni <ammarrahmouni8@gmail.com>
2025-07-01 02:26:29 +0300

    install media library

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -3,16 +3,20 @@
 namespace App\Models;
 
 use App\Traits\HasSlug;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\Image\Enums\CropPosition;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\InteractsWithMedia;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
     /** @use HasFactory<\Database\Factories\PostFactory> */
-    use HasFactory, HasSlug;
+    use HasFactory, HasSlug, InteractsWithMedia;
 
     protected $fillable = [
         'user_id',
@@ -22,6 +26,16 @@ class Post extends Model
         'is_published',
         'published_at',
     ];
+
+    public const MEDIA_COLLECTION = 'posts';
+
+    // Perform crop on image via media library
+    public function registerMediaConversions(?Media $media = null): void
+    {
+        $this->addMediaConversion('thumb-100')
+            ->crop(100, 100, CropPosition::Center)
+            ->performOnCollections($this::MEDIA_COLLECTION);
+    }
 
     public function writer(): BelongsTo
     {
