dc18c5e5757fe34136bde59a506ee78cbf399477
arRahmouni <ammarrahmouni8@gmail.com>
2025-07-29 23:07:42 +0300

    update docker

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,6 +3,11 @@
 # Wait for the DB to be ready (optional but recommended)
 # sleep 10
 
+# Fix permissions at runtime
+chown -R www-data:www-data /var/www/laravel/storage /var/www/laravel/bootstrap/cache
+chmod -R 775 /var/www/laravel/storage /var/www/laravel/bootstrap/cache
+
+
 # Run Laravel migrations and seeders
 php artisan migrate --force
 php artisan db:seed --force
